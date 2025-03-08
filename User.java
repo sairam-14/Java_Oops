9f3d18d9c389d1327e123fad1f2d125e13f37425
sairam-14 <velpulasairam51@gmail.com>
2025-03-08 21:49:56 +0530

    Add files via upload
    Encapsulation

---
+++ User.java
@@ -0,0 +1,24 @@
+package com.encapsulation;
+
+public class User {
+
+	private String name;
+	private String pwd;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+}
